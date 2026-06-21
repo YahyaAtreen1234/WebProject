971f8f9b3009e1e610fdc5b60a6eb7470092b7c5
Yahya Atreen <yahyaatreen09876@gmail.com>
2026-06-21 05:00:52 -0700

    Make Stripe initialization lazy to allow build without env vars

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,12 +1,15 @@
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not set');
-}
+const getStripe = () => {
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error('STRIPE_SECRET_KEY is not set');
+  }
+  return new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2023-10-16',
+  });
+};
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
-});
+export const stripe = process.env.STRIPE_SECRET_KEY ? getStripe() : (null as any);
 
 export interface CheckoutSessionData {
   orderId: string;
